87bfb3fab2026963df1d3520b479965552f7bfa6
Uliana Stefanishyna <uliana.stefanishyna@synapse.com>
2019-08-14 16:02:50 +0300

    task's solution

--- src/main/java/com/agileengine/task/solution/FindTargetElements.java
+++ src/main/java/com/agileengine/task/solution/FindTargetElements.java
@@ -13,24 +13,51 @@ import java.util.stream.Collectors;
 
 public class FindTargetElements {
 
-
     private static void parse(String originalFilePath, String diffFIlePass) throws IOException {
         Document originalDoc = Jsoup.connect(originalFilePath).get();
         Document diffDoc = Jsoup.connect(diffFIlePass).get();
         String elementId = "make-everything-ok-button";
 
-        Optional<Element> elementById = findElementById(originalDoc, elementId);
+        Optional<Element> elementById = Optional.of(originalDoc.getElementById(elementId));
 
         Optional<String> attributesOpt = elementById.map(button ->
                 button.attributes().asList().stream()
                         .map(attr -> attr.getKey() + " = " + attr.getValue())
                         .collect(Collectors.joining(", "))
         );
-        String[] ss = attributesOpt.map(attrs -> attrs.split(",\\s")).orElseGet(() -> new String[1]);
-        Map<String, String> attributes = Arrays.stream(ss)
+        String[] attributesKeyValueArray = attributesOpt.map(attrs -> attrs.split(",\\s"))
+                .orElseGet(() -> new String[1]);
+        Map<String, String> attributes = Arrays.stream(attributesKeyValueArray)
                 .collect(Collectors.toMap(a -> a.split(" = ")[0], b -> b.split(" = ")[1]));
 
-        searchForElement(diffDoc, attributes);
+        System.out.println("Found path : " + searchForElement(diffDoc, attributes));
+    }
+
+    private static StringBuilder searchForElement(Document document, Map<String, String> attributes) {
+        StringBuilder path = new StringBuilder();
+        attributes.forEach((k, v) -> {
+            Elements elementsByAttribute = document.getElementsByAttribute(k);
+
+            elementsByAttribute.stream()
+                    .filter(element -> element.attributes().asList()
+                            .stream()
+                            .anyMatch(attribute -> attribute.getKey().equals(k) && attribute.getValue().equals(v)))
+                    .forEach(foundElement -> path.append(getElementPath(foundElement)));
+        });
+        return path;
+    }
+
+    private static StringBuilder getElementPath(Element element) {
+        StringBuilder path = new StringBuilder();
+        Element parent = element.parent();
+        path.append(element.tag().getName()).append(" > ");
+        while (parent != null) {
+            parent = parent.parent();
+            path.append(Optional.ofNullable(parent)
+                    .map(p -> p.tag().getName() + " > ")
+                    .orElse(""));
+        }
+        return path;
     }
 
     public static void main(String[] args) throws IOException {
@@ -42,32 +69,5 @@ public class FindTargetElements {
                 .orElseThrow(() -> new RuntimeException("Link to the diff file is not provided"));
 
         parse(original, diff);
-    }
-
-    private static Optional<Element> findElementById(Document document, String targetElementId) {
-        return Optional.of(document.getElementById(targetElementId));
-    }
-
-    private static void searchForElement(Document document, Map<String, String> attributes) {
-        attributes.forEach((k, v) -> {
-            Elements elementsByAttribute = document.getElementsByAttribute(k);
-
-            elementsByAttribute.stream()
-                    .filter(element -> element.attributes().asList()
-                            .stream()
-                            .anyMatch(attribute -> attribute.getKey().equals(k) && attribute.getValue().equals(v)))
-                    .forEach(foundElement -> {
-                        StringBuilder path = new StringBuilder();
-                        Element parent = foundElement.parent();
-                        path.append(foundElement.tag().getName()).append(" > ");
-                        while (parent != null) {
-                            parent = parent.parent();
-                            path.append(Optional.ofNullable(parent)
-                                    .map(p -> p.tag().getName() + " > ")
-                                    .orElse(""));
-                        }
-                        System.out.println("Path to found element = " + path);
-                    });
-        });
     }
 }
