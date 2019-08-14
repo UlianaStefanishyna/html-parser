4846aea9df191f084cb08b40e3259cb73b1fa2c5
Uliana Stefanishyna <uliana.stefanishyna@synapse.com>
2019-08-14 16:00:16 +0300

    final solution

--- src/main/java/com/agileengine/task/solution/FindTargetElements.java
+++ src/main/java/com/agileengine/task/solution/FindTargetElements.java
@@ -1,11 +1,9 @@
-package com.agileengine;
+package com.agileengine.task.solution;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -15,7 +13,6 @@ import java.util.stream.Collectors;
 
 public class FindTargetElements {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(JsoupFindByIdSnippet.class);
 
     private static void parse(String originalFilePath, String diffFIlePass) throws IOException {
         Document originalDoc = Jsoup.connect(originalFilePath).get();
@@ -23,23 +20,26 @@ public class FindTargetElements {
         String elementId = "make-everything-ok-button";
 
         Optional<Element> elementById = findElementById(originalDoc, elementId);
-        Optional<String> stringifiedAttributesOpt = elementById.map(button ->
+
+        Optional<String> attributesOpt = elementById.map(button ->
                 button.attributes().asList().stream()
                         .map(attr -> attr.getKey() + " = " + attr.getValue())
                         .collect(Collectors.joining(", "))
         );
-        String[] ss = stringifiedAttributesOpt.map(attrs -> {
-            return attrs.split(",\\s");
-
-        }).orElseGet(() -> new String[1]);
+        String[] ss = attributesOpt.map(attrs -> attrs.split(",\\s")).orElseGet(() -> new String[1]);
         Map<String, String> attributes = Arrays.stream(ss)
                 .collect(Collectors.toMap(a -> a.split(" = ")[0], b -> b.split(" = ")[1]));
+
         searchForElement(diffDoc, attributes);
     }
 
     public static void main(String[] args) throws IOException {
-        String original = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-0-origin.html";
-        String diff = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-2-container-and-clone.html";
+
+        String original = Optional.ofNullable(args[0])
+                .orElseThrow(() -> new RuntimeException("Link to the original file is not provided"));
+
+        String diff = Optional.ofNullable(args[1])
+                .orElseThrow(() -> new RuntimeException("Link to the diff file is not provided"));
 
         parse(original, diff);
     }
@@ -51,7 +51,23 @@ public class FindTargetElements {
     private static void searchForElement(Document document, Map<String, String> attributes) {
         attributes.forEach((k, v) -> {
             Elements elementsByAttribute = document.getElementsByAttribute(k);
-            elementsByAttribute.stream().filter(element -> Optional.of(element.getElementById(v)).isPresent()).forEach(System.out::println);
+
+            elementsByAttribute.stream()
+                    .filter(element -> element.attributes().asList()
+                            .stream()
+                            .anyMatch(attribute -> attribute.getKey().equals(k) && attribute.getValue().equals(v)))
+                    .forEach(foundElement -> {
+                        StringBuilder path = new StringBuilder();
+                        Element parent = foundElement.parent();
+                        path.append(foundElement.tag().getName()).append(" > ");
+                        while (parent != null) {
+                            parent = parent.parent();
+                            path.append(Optional.ofNullable(parent)
+                                    .map(p -> p.tag().getName() + " > ")
+                                    .orElse(""));
+                        }
+                        System.out.println("Path to found element = " + path);
+                    });
         });
     }
 }
